210206e924af9bec7f2bafb5c4d93d21ff1e74e7
SergeySerg123 <sergey_chizhik@ukr.net>
2020-05-04 00:43:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const playBtn = document.querySelector('')
+const playBtn = document.querySelector('#meh');
+
+playBtn.addEventListener('click', () => {
+    const a = new Audio('./assets/sounds/meh.mp3');
+    a.play();
+});
 
 document.addEventListener('DOMContentLoaded', () => {
     const main = document.querySelector('#main');
